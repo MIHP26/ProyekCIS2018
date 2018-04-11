5fe3c5ebcb0afa47d38c43304b37c4fa62418299
Tazkianida <Tazkianida@10.5.89.136>
2018-04-11 12:56:05 +0700

    Bin to Hex and Vice Versa

---
+++ src/BinToHex.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.xml.bind.DatatypeConverter;
+
+public class BinToHex
+{
+    public static String toHexString(byte [] array) {
+        return DatatypeConverter.printHexBinary(array);
+    }
+
+   
+    public static void main(final String[] args) throws IOException
+    {
+        try
+        {
+            FileInputStream fis = new FileInputStream("C:\\Users\\Tazkianida\\workspace\\TesCIS\\src\\gambar.jpg");
+            BufferedWriter fos = new BufferedWriter(new FileWriter("C:\\Users\\Tazkianida\\workspace\\TesCIS\\src\\gambar1.jpg"));
+
+            byte[] bytes = new byte[800];
+            int value = 0;
+            do
+            {
+                value = fis.read(bytes);
+                fos.write(toHexString(bytes));
+
+            }while(value != -1);
+
+            fos.flush();
+            fos.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
