fefef41c93902384cb3239fe9c873ec4fe6b7f5b
Tazkianida <Tazkianida@10.5.89.56>
2018-04-17 12:10:53 +0700

    komit

--- src/Gui.java
+++ src/Gui.java
@@ -102,9 +102,11 @@ public class Gui
             public void actionPerformed (ActionEvent arg0)
             {
                 // TODO Auto-generated method stub
-                Encrypt keyTest = new Encrypt ();   
+                Encrypt encryption = new Encrypt ();   
                 //tes kunci 
-                keyTest.keyProcessing (keyPath);
+                /*encryption.keyProcessing (keyPath);*/
+                
+                
             }
             
         });
